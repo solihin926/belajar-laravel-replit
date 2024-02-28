a8f42b03dc76f60d978452d8d70a08e2e984f865
zack926 <solihin@fmipa.untan.ac.id>
2024-02-28 14:53:29 +0700

    hapus welcome
    hps welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -470,7 +470,7 @@
                         </div>
                     </div> --}}
 
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                    {{-- <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
@@ -489,7 +489,7 @@
                                 tutorials.
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                         <div class="flex items-center">
